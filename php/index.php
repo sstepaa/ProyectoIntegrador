9171056daec72923fbcea0663215700dd386f3eb
Seba <stepaniuk.ar@gmail.com>
2022-04-14 00:32:47 -0300

    Retoque del codigo php , y clases e Ids en css y html

--- php/index.php
+++ php/index.php
@@ -1,25 +1,9 @@
 <?php
-$to = "info@hotmail.com";
-$subject = "Mail desde el formulario";
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$comentarios = $_POST['comentarios'];
-$menu = $_POST['menu'];
-$message = "
-<html>
-<head>
-<title>HTML</title>
-</head>
-<body>
-<h1>Información del formulario</h1>
-<p>Nombre: $nombre </p>
-<p>Email: $email </p>
-<p>Comentarios: $comentarios </p>
-<p>Menú: $menu </p>
-</body>
-</html>";
- echo 'Gracias por comunicarse con nosotros';
-mail($to, $subject, $message, $headers);
+    $nombre = $_GET['Nombre'];
+    $email = $_GET['Email'];
+    $comentarios = $_GET['Comentarios'];
+
+    echo "<h2>Información recibida desde Php</h2>";
+    echo "<p>El nombre recibido es :</p>" , $nombre;
+
 ?>
